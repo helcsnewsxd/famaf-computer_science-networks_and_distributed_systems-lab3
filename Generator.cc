7eaa1f7e9d6581870c7f274ee3743df7c25981e0
Facundo Alvarez Miranda <facualvarez@mi.unc.edu.ar>
2023-05-03 21:19:25 -0300

    Modificaciones necesarias en el código para analisis de casos de estudio.

--- Generator.cc
+++ Generator.cc
@@ -8,7 +8,7 @@ using namespace omnetpp;
 
 class Generator : public cSimpleModule {
 private:
-    cMessage *sendMsgEvent;
+    cPacket *sendPacketEvent;
     cStdDev transmissionStats;
 public:
     Generator();
@@ -21,20 +21,21 @@ protected:
 Define_Module(Generator);
 
 Generator::Generator() {
-    sendMsgEvent = NULL;
+    sendPacketEvent = NULL;
 
 }
 
 Generator::~Generator() {
-    cancelAndDelete(sendMsgEvent);
+    cancelAndDelete(sendPacketEvent);
 }
 
 void Generator::initialize() {
     transmissionStats.setName("TotalTransmissions");
-    // create the send packet
-    sendMsgEvent = new cMessage("sendEvent");
+// create the send packet
+    sendPacketEvent = new cPacket("sendPacket"); // Change packet name and data type
+    sendPacketEvent->setByteLength(par("packetByteSize")); // Set byte length of the packet
     // schedule the first event at random time
-    scheduleAt(par("generationInterval"), sendMsgEvent);
+    scheduleAt(par("generationInterval"), sendPacketEvent); 
 }
 
 void Generator::finish() {
@@ -43,14 +44,14 @@ void Generator::finish() {
 void Generator::handleMessage(cMessage *msg) {
 
     // create new packet
-    cMessage *pkt = new cMessage("packet");
+    cPacket *pkt = new cPacket("packet"); // Change data type to cPacket*
+    pkt->setByteLength(par("packetByteSize")); // Set byte length of the packet
     // send to the output
     send(pkt, "out");
-
     // compute the new departure time
     simtime_t departureTime = simTime() + par("generationInterval");
     // schedule the new packet generation
-    scheduleAt(departureTime, sendMsgEvent);
+    scheduleAt(departureTime, sendPacketEvent);
 }
 
 #endif /* GENERATOR */
--- Queue.cc
+++ Queue.cc
@@ -9,8 +9,11 @@ using namespace omnetpp;
 class Queue: public cSimpleModule {
 private:
     cQueue buffer;
-    cMessage *endServiceEvent;
+    cPacket *endServiceEvent;
+    cOutVector bufferSizeVector;
+    cOutVector packetDropVector;
     simtime_t serviceTime;
+
 public:
     Queue();
     virtual ~Queue();
@@ -32,33 +35,47 @@ Queue::~Queue() {
 
 void Queue::initialize() {
     buffer.setName("buffer");
-    endServiceEvent = new cMessage("endService");
+    endServiceEvent = new cPacket("endService");
+    bufferSizeVector.setName("bufferSize");
+    packetDropVector.setName("packetsDropped");
 }
 
 void Queue::finish() {
 }
 
-void Queue::handleMessage(cMessage *msg) {
 
+void Queue::handleMessage(cMessage *msg) {
+    int drops = 0;
     // if msg is signaling an endServiceEvent
     if (msg == endServiceEvent) {
         // if packet in buffer, send next one
         if (!buffer.isEmpty()) {
             // dequeue packet
-            cMessage *pkt = (cMessage*) buffer.pop();
+            cPacket *pkt = (cPacket*) buffer.pop();
             // send packet
             send(pkt, "out");
             // start new service
-            serviceTime = par("serviceTime");
+            serviceTime = pkt->getDuration();
             scheduleAt(simTime() + serviceTime, endServiceEvent);
         }
-    } else { // if msg is a data packet
-        // enqueue the packet
-        buffer.insert(msg);
-        // if the server is idle
-        if (!endServiceEvent->isScheduled()) {
-            // start the service
-            scheduleAt(simTime(), endServiceEvent);
+    }
+    else { // if msg is a data packet
+        //check buffer limit
+        if (buffer.getLength() >= par("bufferSize").intValue()) {
+            // drop the packet
+            delete msg;
+            this->bubble("packet dropped");
+            packetDropVector.record(1);
+        }
+        else {
+            // enqueue the packet
+            buffer.insert(msg);
+            bufferSizeVector.record(buffer.getLength());
+            // if the server is idle
+            if (!endServiceEvent->isScheduled()) {
+                // start the service
+                scheduleAt(simTime(), endServiceEvent);
+            }
         }
     }
 }
